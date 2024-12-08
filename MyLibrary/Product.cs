24035c00d6f2b89499abf4bfa7c6a1408afcdb71
Alexandro Yule <alexandroyule@gmail.com>
2024-12-08 17:40:12 -0500

    Creating a Constructor in Product Class

--- MyLibrary/Product.cs
+++ MyLibrary/Product.cs
@@ -6,5 +6,13 @@
         public string Name { get; set; }
         public decimal UnitPrice { get; set; }
         public int UnitInStock { get; set; }
+    
+        public Product(int id, string name, decimal unitPrice, int unitInStock) 
+        { 
+            Id = id;
+            Name = name;
+            UnitPrice = unitPrice;
+            UnitInStock = unitInStock;
+        }
     }
 }
