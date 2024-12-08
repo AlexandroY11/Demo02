ae836efd64f4b7a2e19afdb9872729a11573b614
Alexandro Yule <alexandroyule@gmail.com>
2024-12-08 17:34:32 -0500

    Product Properties

---
+++ MyLibrary/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace MyLibrary
+{
+    public class Product
+    {
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private decimal UnitPrice { get; set; }
+        private int UnitInStock { get; set; }
+    }
+}
