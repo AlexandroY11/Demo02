12e4203b078e3561d10723985b82838f9c92b71a
Alexandro Yule <alexandroyule@gmail.com>
2024-12-08 18:31:55 -0500

    Creating Employee Class

---
+++ MyLibrary/Employee.cs
@@ -0,0 +1,20 @@
+﻿namespace MyLibrary
+{
+    public abstract class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public abstract decimal CalculateMonthlyPayment();
+    }
+
+    public class Secretary : Employee
+    {
+        public override decimal CalculateMonthlyPayment() => 15000;
+    }
+
+    public class Manager : Employee
+    {
+        public override decimal CalculateMonthlyPayment() => 35000;
+        
+    }
+}
